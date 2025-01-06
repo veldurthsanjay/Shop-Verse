014e9347f3abf96e4fab63de43bf5cf6cb19c554
sanjay <veldurthsanjay@gmail.com>
2025-01-06 21:31:26 +0530

    cart count updated in login form

--- Assets/JS/login.js
+++ Assets/JS/login.js
@@ -33,3 +33,8 @@
     }
 })  
 
+function updateCartCount() {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    document.getElementById('cart-count').innerText = ` (${cart.length})`;
+  }
+  document.addEventListener('DOMContentLoaded', updateCartCount);
